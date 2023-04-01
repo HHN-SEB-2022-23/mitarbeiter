ceed8115914f1ac92ca8e705b27e08b0f4eea9e5
Frank-Mayer <git@frank-mayer.io>
2023-04-01 16:30:12 +0200

    main method

--- src/main/java/de/hhn/Main.java
+++ src/main/java/de/hhn/Main.java
@@ -1,7 +1,23 @@
 package de.hhn;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        var rene = new Angestellter("Rene", "Ott");
+        var mitarbeiter = Arrays.asList(
+                new Angestellter("Rene", "Ott"),
+                new ATAngestellter("Jan", "Jäger"),
+                new Zeitarbeiter("Felix", "Marzioch")
+        );
+
+        for (var m : mitarbeiter) {
+            System.out.printf(
+                "%s <%s> %s€ %s€%n",
+                m,
+                m.getClass().getSimpleName(),
+                m.entgeltBerechnen(),
+                m.getJahresGehaltBisHeute()
+            );
+        }
     }
 }
