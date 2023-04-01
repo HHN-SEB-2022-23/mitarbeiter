a8e97aa87d61680711c3e80ccddfaeaad80fc186
René Ott <rene.ott1997@yahoo.de>
2023-04-01 16:05:08 +0200

    Angestellter erstellt

--- src/main/java/de/hhn/Angestellter.java
+++ src/main/java/de/hhn/Angestellter.java
@@ -1,2 +1,38 @@
-package de.hhn;public class Angestellter {
+package de.hhn;
+
+/**
+ * @author René Ott
+ */
+
+public class Angestellter extends Mitarbeiter{
+
+    private float monatsLohn;
+    private float ueberStundenTarif;
+    private int gearbeiteteUeberstunden;
+
+    public Angestellter(){
+
+    }
+
+    public float getMonatsLohn(){
+        return monatsLohn;
+    }
+
+    public float getUeberStundenTarif(){
+        return ueberStundenTarif;
+    }
+
+    public int getGearbeiteteUeberstunden(){
+        return gearbeiteteUeberstunden;
+    }
+
+    public void setGearbeiteteUeberstunden(int x){
+        gearbeiteteUeberstunden = x;
+    }
+
+    @Override
+    public float entgeltBerechnen(){
+        float entgelt = monatsLohn + (gearbeiteteUeberstunden * ueberStundenTarif);
+        return entgelt;
+    }
 }
