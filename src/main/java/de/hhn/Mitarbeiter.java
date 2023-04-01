2ad699f4a9ab2dae27d6707461260a6b358682d0
Frank-Mayer <git@frank-mayer.io>
2023-04-01 15:30:40 +0200

    added Mitarbeiter class

---
+++ src/main/java/de/hhn/Mitarbeiter.java
@@ -0,0 +1,31 @@
+package de.hhn;
+
+public abstract class Mitarbeiter {
+    private String vorname = "";
+    private String nachname = "";
+    private float jahresGehaltBisHeute = 0;
+
+    public Mitarbeiter() { }
+
+    public String getVorname() {
+        return this.vorname;
+    }
+
+    public String getNachname() {
+        return this.nachname;
+    }
+
+    public float getJahresGehaltBisHeute() {
+        return this.jahresGehaltBisHeute;
+    }
+
+    public abstract float entgeldBerechnen();
+
+    @Override public String toString() {
+        return "Mitarbeiter{" +
+            "vorname='" + this.vorname + '\'' +
+            ", nachname='" + this.nachname + '\'' +
+            ", jahresGehaltBisHeute=" + this.jahresGehaltBisHeute +
+            '}';
+    }
+}
