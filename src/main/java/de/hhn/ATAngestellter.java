d5fe7c460b6c87c8659f27d5c7b4527c3104bb6a
Jan-Niklas <jan-niklas.jaeger@ejbk.de>
2023-04-07 21:11:35 +0200

    merge commit fixed

--- src/main/java/de/hhn/ATAngestellter.java
+++ src/main/java/de/hhn/ATAngestellter.java
@@ -1,24 +1,42 @@
 package de.hhn;
 
 /**
-* @author Jan-Niklas Jäger
-*/
+ * @author Jan-Niklas Jäger
+ */
 public class ATAngestellter extends Mitarbeiter {
 
     private float monatsLohn;
+    private float jahresGehaltBisHeute = 0;
+    private String vorname;
+    private String nachname;
 
-    public ATAngestellter(String vorname, String nachname, float monatsLohn){
-    super(vorname,nachname);
-    this.monatsLohn = monatsLohn;
+    public ATAngestellter(String vorname, String nachname, float monatsLohn) {
+        this.vorname = vorname;
+        this.nachname = nachname;
+        this.monatsLohn = monatsLohn;
     }
 
+<<<<<<< Updated upstream
     public float getMonatsLohn(){
+=======
+
+    public float getMonatsLohn() {
+>>>>>>> Stashed changes
         return monatsLohn;
     }
+
     @Override
 
-    public float entgeltBerechnen(){
+    public float entgeltBerechnen() {
         jahresGehaltBisHeute += monatsLohn;
         return monatsLohn;
+    }
+
+    public float tatsächlicheEinkommensSteuer(){
+
+    }
+
+    public float vorraussichtlicheEinkommmensSteuer(){
+
     }
 }
