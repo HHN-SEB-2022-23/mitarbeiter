322e9a61a509b66ebb94bad8bd4bdbca9f8c6e10
Frank-Mayer <git@frank-mayer.io>
2023-04-07 21:37:03 +0200

    toString in vertragsArtT

--- src/main/java/de/hhn/vertragsArtT.java
+++ src/main/java/de/hhn/vertragsArtT.java
@@ -1,7 +1,18 @@
 package de.hhn;
 
 public enum vertragsArtT {
-    ATANGESTELLTER,
-    ZEITARBEITER,
-    ANGESTELLTER,
+    ATANGESTELLTER("AT Angestellter"),
+    ZEITARBEITER("Zeitarbeiter"),
+    ANGESTELLTER("Angestellter");
+
+    private final String display;
+
+    vertragsArtT(String display) {
+        this.display = display;
+    }
+
+    @Override
+    public String toString() {
+        return display;
+    }
 }
