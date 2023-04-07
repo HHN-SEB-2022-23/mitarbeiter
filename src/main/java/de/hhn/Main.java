b902116b883610113ebdfea946ce791386d954bd
Frank-Mayer <git@frank-mayer.io>
2023-04-07 22:03:04 +0200

    main method

--- src/main/java/de/hhn/Main.java
+++ src/main/java/de/hhn/Main.java
@@ -6,18 +6,18 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        List<IMitarbeiter> mitarbeiter = new ArrayList<>(3);
+        List<ISteuerZahler> mitarbeiter = new ArrayList<>(3);
         mitarbeiter.add(new Angestellter("Rene", "Ott", 20000, 20, 4));
         mitarbeiter.add(new ATAngestellter("Jan", "Jäger", 20200));
         mitarbeiter.add(new Zeitarbeiter("Felix", "Marzioch", 25, 400));
 
         for (var m : mitarbeiter) {
             System.out.printf(
-                "%s <%s> %s€ %s€%n",
+                "%s <%s> tatsächlicheEinkommenSteuer: %s voraussichtlicheEinkommenSteuer: %s€%n",
                 m,
                 m.getClass().getSimpleName(),
-                m.entgeltBerechnen(),
-                m.getJahresGehaltBisHeute()
+                m.tatsächlicheEinkommenSteuer(),
+                m.voraussichtlicheEinkommenSteuer()
             );
         }
     }
